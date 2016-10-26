a6cca454925276cdcd6c629261b8296a3f3fcdfd
Pepyn Swagemakers <ps222nq@student.lnu.se>
2016-10-26 18:02:10 +0200

    working on register form validation

--- controller/RegistrationController.php
+++ controller/RegistrationController.php
@@ -6,4 +6,37 @@ require_once("model/Database.php");
 
 class RegistrationController {
 
+    private $data;
+    private $validPassword = false;
+    private $validUsername = false;
+
+    public function __construct($formData)
+    {
+        $this->data = $formData;
+    }
+
+    public function isUserNameValid()
+    {
+        return $this->validUsername;
+    }
+
+    public function isPasswordValid()
+    {
+        return $this->validPassword;
+    }
+
+    public function validateRegistrationForm()
+    {
+
+    }
+
+    private function validateUserName($username) {
+
+    }
+
+    private function validatePassword($password) {
+
+    }
+
+
 }
