aa6bebb30fe8be193ef75f768f328f018003e2d3
Pepyn Swagemakers <ps222nq@student.lnu.se>
2016-11-11 19:50:32 +0100

    updating running instructions

--- model/Database.php
+++ model/Database.php
@@ -8,8 +8,19 @@ class Database {
     //See also Clean Code (Martin), Chapter 8: Boundaries - "Using code that does not yet exist"
 
     //TODO: remove these later, placeholder for testing login functionality w/o DB
-    private $testUser = "Admin";
-    private $testPass = "Password";
+    private $testUser;
+    private $testPass;
+    private $settings;
+
+    public function __construct()
+    {
+        require_once("model/DBSettings.php");
+        $this->settings = new DBSettings();
+
+        $this->testUser = $this->settings->username;
+        $this->testPass = $this->settings->password;
+
+    }
 
     public function authenticateUser($username, $password)
     {
